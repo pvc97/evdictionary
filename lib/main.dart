d07512c8cd14c0b3d64a9583e43033621b355d1f
pvc2209 <cuongpv2209@gmail.com>
2021-03-25 17:03:40 +0700

    Remove Dismiss key board when press outside textfield in main.dart - It didn't work as my expected

--- lib/main.dart
+++ lib/main.dart
@@ -15,35 +15,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Listener(
-      // Dismiss key board when press outside textfield
-      onPointerDown: (_) {
-        FocusScopeNode currentFocus = FocusScope.of(context);
-        if (!currentFocus.hasPrimaryFocus &&
-            currentFocus.focusedChild != null) {
-          currentFocus.focusedChild.unfocus();
-        }
-      },
-      child: MaterialApp(
-        title: 'EV DICTIONARY',
-        theme: ThemeData(
-          scaffoldBackgroundColor: Colors.white,
-          primaryColor: kPrimaryColor,
-          textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: FutureBuilder(
-          future: _loadDatabase(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Navigation();
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
+    return MaterialApp(
+      title: 'EV DICTIONARY',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primaryColor: kPrimaryColor,
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: FutureBuilder(
+        future: _loadDatabase(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Navigation();
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
 
-            return LoadingScreen();
-          },
-        ),
+          return LoadingScreen();
+        },
       ),
     );
   }
